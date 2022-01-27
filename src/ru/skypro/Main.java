b9c0410a653eccbe13711be7edb268f73e51db00
mativaclub <96348077+mativaclub@users.noreply.github.com>
2022-01-27 17:26:58 +0200

    homework

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,16 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Book bookNumberOne = new Book("First book",
-                new Author("Name", "Surname"), 2017);
-        System.out.println("bookNumberOne.titleOfBook = " + bookNumberOne.getTitleOfBook());
-        System.out.println("bookNumberOne.nameOfAuthor = "
-                + bookNumberOne.getAuthor().getNameOfAuthor() + " "
-                + bookNumberOne.getAuthor().getSurnameOfAuthor());
-        System.out.println("bookNumberOne.yearOfPublication = " + bookNumberOne.getYearOfPublication());
-        bookNumberOne.setYearOfPublication(2020);
-        System.out.println("bookNumberOne.getYearOfPublication() = " + bookNumberOne.getYearOfPublication());
-       // Book bookNumberTwo = new Book("Book Number Two", "Someone Number Two", 2017);
+        Book book1 = new Book("Иди туда где страшно",
+                new Author("Джим", "Лоулесс"), 2017);
+        System.out.println("book1.titleOfBook = " + book1.getTitleOfBook());
+        System.out.println("book1.nameOfAuthor = "
+                + book1.getAuthor().getNameOfAuthor() + " "
+                + book1.getAuthor().getSurnameOfAuthor());
+        System.out.println("book1.yearOfPublication = " + book1.getYearOfPublication());
+        book1.setYearOfPublication(2020);
+        System.out.println("book1.getYearOfPublication() = " + book1.getYearOfPublication());
+
+        Book book2 = new Book("Исповедь экономического убийцы",
+                new Author("Джон", "Перкинс"), 2018);
+        System.out.println("book2.titleOfBook = " + book2.getTitleOfBook());
+        System.out.println("book2.nameOfAuthor = "
+                + book2.getAuthor().getNameOfAuthor() + " "
+                + book2.getAuthor().getSurnameOfAuthor());
+        System.out.println("book2.yearOfPublication = " + book2.getYearOfPublication());
 
     }
 }
